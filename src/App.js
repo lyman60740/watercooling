7759a70570b74139e5a3ab9e5117049f24c44833
Lyman <lym_60740@hotmail.fr>
2024-09-04 11:51:41 +0200

    test violet bleu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { MeshTransmissionMaterial, Environment, OrbitControls } from '@react-three/drei';
-import { EffectComposer, Bloom } from '@react-three/postprocessing';
-import { useControls } from 'leva';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import * as THREE from 'three';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import {
+  MeshTransmissionMaterial,
+  Environment,
+  OrbitControls,
+} from "@react-three/drei";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import { useControls } from "leva";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import * as THREE from "three";
+import "./App.css";
 
 // Enregistrer le plugin ScrollTrigger avec GSAP
 gsap.registerPlugin(ScrollTrigger);
@@ -16,19 +20,16 @@ function TubeWithLiquid({ curve }) {
   const liquidRef = useRef();
   const progressRef = useRef(0);
 
-  const {
-    thickness,
-    roughness,
-    metalness,
-    transmission,
-    color,
-  } = useControls('Tube Material', {
-    thickness: { value: 0, min: 0, max: 5 },
-    roughness: { value: 0.13, min: 0, max: 1 },
-    metalness: { value: 0.24, min: 0, max: 1 },
-    transmission: { value: 1, min: 0, max: 1 },
-    color: 'gray',
-  });
+  const { thickness, roughness, metalness, transmission, color } = useControls(
+    "Tube Material",
+    {
+      thickness: { value: 0, min: 0, max: 5 },
+      roughness: { value: 0.13, min: 0, max: 1 },
+      metalness: { value: 0.24, min: 0, max: 1 },
+      transmission: { value: 1, min: 0, max: 1 },
+      color: "#cc00ff",
+    }
+  );
 
   const {
     thickness: liquidThickness,
@@ -38,36 +39,44 @@ function TubeWithLiquid({ curve }) {
     ior: liquidIor,
     anisotropy: liquidAnisotropy,
     color: liquidColor,
-  } = useControls('Liquid Material', {
+  } = useControls("Liquid Material", {
     thickness: { value: 0.5, min: 0, max: 5 },
     roughness: { value: 0.11, min: 0, max: 1 },
     metalness: { value: 1, min: 0, max: 1 },
     transmission: { value: 0, min: 0, max: 1 },
     ior: { value: 1.5, min: 1, max: 2.333 },
     anisotropy: { value: 0.5, min: 0, max: 1 },
-    color: '#ffc600',
+    color: "#00b2ff",
   });
 
   useEffect(() => {
     // Configuration de ScrollTrigger pour animer la progression avec le défilement
     gsap.to(progressRef, {
       current: 1,
-      ease: 'none',
+      ease: "none",
       scrollTrigger: {
         trigger: ".watercooling-main",
         start: "top top",
         end: "bottom bottom",
         scrub: 4, // Synchroniser avec le défilement
-      }
+      },
     });
   }, []);
 
   useFrame(() => {
     const progress = progressRef.current;
-    const points = curve.getSpacedPoints(1000).slice(0, Math.floor(1000 * progress));
-    
+    const points = curve
+      .getSpacedPoints(1000)
+      .slice(0, Math.floor(1000 * progress));
+
     if (points.length > 1 && liquidRef.current) {
-      const tubeGeometry = new THREE.TubeGeometry(new THREE.CatmullRomCurve3(points), 100, 0.031, 20, false);
+      const tubeGeometry = new THREE.TubeGeometry(
+        new THREE.CatmullRomCurve3(points),
+        100,
+        0.031,
+        20,
+        false
+      );
       liquidRef.current.geometry = tubeGeometry;
 
       // Mettre à jour l'opacité en fonction de la progression
@@ -109,8 +118,10 @@ function TubeWithLiquid({ curve }) {
 }
 
 function App() {
-  const directionalLight = useControls('Directional Light', {
-    x: -0.3, y: 0, z: 2.2,
+  const directionalLight = useControls("Directional Light", {
+    x: -0.3,
+    y: 0,
+    z: 2.2,
   });
 
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -121,63 +132,96 @@ function App() {
       const { innerWidth, innerHeight } = window;
       const mouseX = (event.clientX / innerWidth - 0.5) * 50;
       const mouseY = (event.clientY / innerHeight - 0.5) * 50;
-      
+
       setMousePosition({ x: mouseX, y: mouseY });
     };
 
-    window.addEventListener('mousemove', handleMouseMove);
-    
+    window.addEventListener("mousemove", handleMouseMove);
+
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
+
+  const [baseColor, normalMap, roughnessMap, metalnessMap, aoMap] = useLoader(
+    THREE.TextureLoader,
+    [
+      "/Metal_006_SD/Metal_006_basecolor.jpg",
+      "/Metal_006_SD/Metal_006_normal.jpg",
+      "/Metal_006_SD/Metal_006_roughness.jpg",
+      "/Metal_006_SD/Metal_006_metallic.jpg",
+      "/Metal_006_SD/Metal_006_ambientOcclusion.jpg",
+    ]
+  );
 
   const curves = [
     new THREE.CubicBezierCurve3(
       new THREE.Vector3(-2, 1.5, 0),
       new THREE.Vector3(-1, -1, 0),
       new THREE.Vector3(1, 1, 0),
-      new THREE.Vector3(2, -1.5, 0),
+      new THREE.Vector3(2, -1.5, 0)
     ),
     new THREE.CubicBezierCurve3(
       new THREE.Vector3(2, 1.5, 0),
       new THREE.Vector3(1, -1, 0),
       new THREE.Vector3(-1, 1, 0.5),
-      new THREE.Vector3(-2, -1.5, 0),
+      new THREE.Vector3(-2, -1.5, 0)
     ),
     new THREE.CubicBezierCurve3(
       new THREE.Vector3(-0.5, 1.5, 0),
       new THREE.Vector3(-1.5, -1, 1),
       new THREE.Vector3(-0.3, 1, -1),
-      new THREE.Vector3(-0.3, -1.5, 0),
+      new THREE.Vector3(-0.3, -1.5, 0)
     ),
     new THREE.CubicBezierCurve3(
       new THREE.Vector3(0.5, 1.5, 0),
       new THREE.Vector3(1.5, -1, 1),
       new THREE.Vector3(0.3, 1, -1),
-      new THREE.Vector3(0.3, -1.5, 0),
+      new THREE.Vector3(0.3, -1.5, 0)
     ),
     // Ajoutez plus de courbes si nécessaire
   ];
 
   return (
     <div className="App">
-      <Canvas className="canvas" style={{ background: 'black' }} camera={{
-        position: [0, 0, 1.8],
-        fov: 75,
-        near: 0.1,
-        far: 1000,
-      }}>
+      <Canvas
+        className="canvas"
+        style={{ background: "black" }}
+        camera={{
+          position: [0, 0, 1.8],
+          fov: 75,
+          near: 0.1,
+          far: 1000,
+        }}
+        gl={{ antialias: false }}
+      >
         <Environment preset="studio" />
         <directionalLight
           position={[
             directionalLight.x + mousePosition.x * 0.1,
             directionalLight.y + mousePosition.y * 0.1,
-            directionalLight.z
+            directionalLight.z,
           ]}
-          intensity={2}
+          intensity={5}
+          color={"#cc00ff"}
         />
-        <directionalLight position={[0 + mousePosition.x * 0.1, 3 + mousePosition.y * 0.1, 2]} intensity={2} />
+        <directionalLight
+          position={[0 + mousePosition.x * 0.1, 3 + mousePosition.y * 0.1, 2]}
+          intensity={5}
+          color={"#cc00ff"}
+        />
+        {/* Plane with metal texture as background */}
+        {/* <mesh position={[0, 0, -10]} rotation={[0, 0, -Math.PI / 2]}>
+          <planeGeometry args={[50, 50]} />
+          <meshStandardMaterial
+            map={baseColor}
+            normalMap={normalMap}
+            roughnessMap={roughnessMap}
+            metalnessMap={metalnessMap}
+            aoMap={aoMap}
+            color={"black"}
+          />
+        </mesh> */}
         {curves.map((curve, index) => (
           <TubeWithLiquid key={index} curve={curve} />
         ))}
@@ -187,9 +231,8 @@ function App() {
             luminanceSmoothing={0.2}
             intensity={0.1}
           />
-          
         </EffectComposer>
-        {/* OrbitControls supprimé */}
+
         {/* <OrbitControls /> */}
       </Canvas>
     </div>
